2cb8c30ba7d838c30673e3978b187f086fc98feb
Vaino <vaino.granat1@gmail.com>
2021-01-27 15:46:04 +0200

    Single speaker demo working with LJSpeec.

--- src/tts.py
+++ src/tts.py
@@ -10,7 +10,8 @@ import torch
 import numpy as np
 import librosa
 import librosa.display
-
+import sounddevice as sd
+import matplotlib.pyplot as plt
 
 NAME = "deepvoice3_pytorch"
 
@@ -18,11 +19,15 @@ NAME = "deepvoice3_pytorch"
 BRANCH = "abf0a21"
 
 # Project root location CHANGE THIS ON NEW SYSTEM!!!
-ROOT = "~/home/vaino/deep-speaking-avatar-text-to-audio"
+ROOT = "/home/vaino/deep-speaking-avatar-text-to-audio/"
 
 # Pretrained model information
 PRESET = "20180505_deepvoice3_ljspeech.json"
 CHECKPOINT = "20180505_deepvoice3_checkpoint_step000640000.pth"
+
+# Alias Declarations
+fs = 22050
+hop_length = 256
 
 """
 This functions sets up the environment to use DeepVoice3 and get model
@@ -31,7 +36,7 @@ def setup():
     os.chdir(ROOT)
     if not exists(NAME):
         url = "https://github.com/r9y9/" + NAME
-        os.system("git clone ‰s" % url)
+        os.system("git clone %s" % url)
     os.chdir(NAME)
     os.system("pip install -q -e \'.[bin]\'") # THIS MIGHT NOT WORK: TEST
     #os.system("./dependency_scipt.sh") # Install python dependcies
@@ -39,7 +44,7 @@ def setup():
     os.system("python -m nltk.downloader cmudict") # English text processing 
 
     # Get the model
-    os.system("git checkout ‰s --quite" % BRANCH)
+    os.system("git checkout %s --quiet" % BRANCH)
 
     if not exists(PRESET):
         url = "https://www.dropbox.com/s/0ck82unm0bo0rxd/" + PRESET
@@ -48,15 +53,64 @@ def setup():
         url = "https://www.dropbox.com/s/5ucl9remrwy5oeg/" + CHECKPOINT
         os.system("curl -O -L %s" % url)
 
+    # Hyper parameters
+    import hparams
+    import json
+
+    # Load parameters from preset
+    with open(PRESET) as f:
+        hparams.hparams.parse_json(f.read())
+
+    # Inject frontend text processor
+    import synthesis
+    import train
+    from deepvoice3_pytorch import frontend
+    synthesis._frontend = getattr(frontend, "en")
+    train._frontend =  getattr(frontend, "en")
+
+    # alises
+    fs = hparams.hparams.sample_rate
+    hop_length = hparams.hparams.hop_size
+    print(os.path.dirname(os.path.realpath(__file__)))
+
     # Load model
     from train import build_model
     from train import restore_parts, load_checkpoint
 
+    print("Building model")
     model = build_model()
     model = load_checkpoint(CHECKPOINT, model, None, True)
     return model
 
-def tts(model, text, p=0, speaker_id=None, fast=True, figures=False):
+def tts(model, text, p=0, speaker_id=None, fast=True, figures=True):
     from synthesis import tts as _tts
-    waveform , alignment, spectorgram, mel = _tts(model, text, p, speaker_id, fast)
-    return waveform
+    waveform, alignment, spectrogram, mel = _tts(model, text, p, speaker_id, fast)
+    if figures:
+        visualize(alignment, spectrogram)
+    sd.play(waveform, 22500)
+    sd.wait()
+
+
+def visualize(alignment, spectrogram):
+  label_fontsize = 16
+  plt.figure(figsize=(16,16))
+
+  plt.subplot(2,1,1)
+  plt.imshow(alignment.T, aspect="auto", origin="lower", interpolation=None)
+  plt.xlabel("Decoder timestamp", fontsize=label_fontsize)
+  plt.ylabel("Encoder timestamp", fontsize=label_fontsize)
+  plt.colorbar()
+
+  plt.subplot(2,1,2)
+  librosa.display.specshow(spectrogram.T, sr=fs, 
+                           hop_length=hop_length, x_axis="time", y_axis="linear")
+  plt.xlabel("Time", fontsize=label_fontsize)
+  plt.ylabel("Hz", fontsize=label_fontsize)
+  plt.tight_layout()
+  plt.colorbar()
+  plt.show()
+
+model = setup()
+text = "I would like to interject for a moment. What you are refering to as Linux, is in fact GNU slash linux. Eat my ass. Hello Mr. Stallman"
+tts(model, text, speaker_id=None ,figures=False)
+print("Done")
