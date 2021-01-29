86c4f86346eaa767df0b7050753b99ec0c3e2e0d
Vaino <vaino.granat1@gmail.com>
2021-01-29 19:01:08 +0200

    Python script to use mozilla tts.

--- src/mzz.sh
+++ src/mzz.sh
@@ -1,5 +1,5 @@
-tts --text "I would like to interject, what you are referring as Linux is actually GNU/Linux" \
+tts --text "Hello Antti nice to meet you" \
     --model_name "tts_models/en/ljspeech/tacotron2-DCA" \
-    --vocoder_name "vocoder_models/universal/libri-tts/wavegrad" \
+    --vocoder_name "vocoder_models/en/ljspeech/mulitband-melgan" \
     --out_path "/home/vaino/synth/" \
     --use_cuda USE_CUDA
