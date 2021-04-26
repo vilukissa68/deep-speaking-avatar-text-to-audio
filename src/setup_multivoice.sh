f9af009c9da280e1359bd85bd19724b0083fb01b
vaino <vaino.granat1@gmail.com>
2021-04-26 16:39:50 +0300

    Updated multivoice setup script.

--- src/setup_multivoice.sh
+++ src/setup_multivoice.sh
@@ -1,6 +1,6 @@
 # Setup TTS
 git clone https://github.com/Edresson/TTS -b dev-gst-embeddings
-#apt-get install espeak
+## Install TTS dependencies
 cd TTS/
 pip install -r requirements.txt
 python setup.py develop
@@ -10,3 +10,9 @@ cd ../
 wget -c -q --show-progress -O ./TTS-checkpoint.zip https://github.com/Edresson/TTS/releases/download/v1.0.0/Checkpoints-TTS-MultiSpeaker-Jia-et-al-2018-with-GST-CorentinJ_SpeakerEncoder_and_DDC.zip
 unzip ./TTS-checkpoint.zip
 wget https://github.com/Edresson/TTS/releases/download/v1.0.0/gst-style-example.wav
+
+# Install espeak
+sudo apt-get install espeak
+
+# Install additional project dependencies
+pip install sounddevice
